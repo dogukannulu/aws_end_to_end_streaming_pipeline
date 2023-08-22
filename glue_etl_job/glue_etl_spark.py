a4ce7a8a05a85b433df7977c099e00a5acbf72e9
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-23 01:39:04 +0300

    initiate glue_etl_job

---
+++ glue_etl_job/glue_etl_spark.py
@@ -0,0 +1,22 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+glue_dynamic_frame_initial = glueContext.create_dynamic_frame.from_catalog(database='glue-etl-from-csv-to-parquet', table_name='ufo_reports_source_csv')
+
+df_spark = glue_dynamic_frame_initial.toDF()
