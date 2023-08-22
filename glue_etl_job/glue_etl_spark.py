710531007ad8c1fc8d8c74d66697ade061f7571e
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-23 02:15:29 +0300

    add jdbc connection

--- glue_etl_job/glue_etl_spark.py
+++ glue_etl_job/glue_etl_spark.py
@@ -20,3 +20,26 @@ job.init(args['JOB_NAME'], args)
 glue_dynamic_frame_initial = glueContext.create_dynamic_frame.from_catalog(database='glue-etl-from-parquet-to-redshift', table_name='parquet_data')
 
 df_spark = glue_dynamic_frame_initial.toDF()
+
+##############################
+# Insert all the transformations here
+# transformed_data
+
+###############################
+
+
+jdbc_url = "jdbc:redshift://your-redshift-cluster-endpoint:5439/your-database"
+redshift_username = "your-username"
+redshift_password = "your-password"
+redshift_table = "target_table_name"
+
+filtered_df.write \
+    .format("com.databricks.spark.redshift") \
+    .option("url", jdbc_url) \
+    .option("dbtable", redshift_table) \
+    .option("user", redshift_username) \
+    .option("password", redshift_password) \
+    .mode("append") \
+    .save()
+
+job.commit()
