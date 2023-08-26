bc50669f72571055dbaac6e49d4e61655b064870
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-26 19:38:27 +0300

    remove parquet converter class

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -15,13 +15,14 @@ class S3Loader:
         Read JSON from an S3 bucket & load into a pandas dataframe
         """
         print("Starting S3 object retrieval process...")
+        
         try:
             get_response = self.s3.get_object(Bucket=bucket_name, Key=key)
             print("Object retrieved from S3 bucket successfully")
         except ClientError as e:
             print(f"S3 object cannot be retrieved: {e}")
             return None
-        
+                
         json_data = get_response['Body'].read().decode('utf-8')
         
         json_data_separated = json_data.replace('}{', '},{')
@@ -31,17 +32,6 @@ class S3Loader:
         df = pd.DataFrame(json_objects)
 
         return df
-
-
-class ParquetConverter:
-    def to_parquet(self, df):
-        """
-        Convert DataFrame to Parquet format stored in memory buffer
-        """
-        parquet_buffer = io.BytesIO()
-        df.to_parquet(parquet_buffer, index=False)
-
-        return parquet_buffer
 
 
 class S3Uploader:
@@ -54,7 +44,11 @@ class S3Uploader:
         """
         Upload Parquet data from memory buffer to S3
         """
-        self.s3.upload_fileobj(buffer, self.target_s3_bucket, self.target_s3_key)
+        try:
+            self.s3.put_object(Body=buffer.getvalue(), Bucket=self.target_s3_bucket, Key=self.target_s3_key)
+            print("Parquet file uploaded into S3 successfully")
+        except Exception as e:
+            print(f"Error occured while uploading parquet into S3: {e}")
         
 
 def lambda_handler(event, context):
@@ -68,22 +62,13 @@ def lambda_handler(event, context):
         s3_loader = S3Loader()
         df = s3_loader.load_df_from_s3(bucket_name=bucket, key=key)
         
-        if df is None:
-            print("Object cannot be retrieved correctly from S3")
-            return {
-                'statusCode': 500,
-                'body': json.dumps('Error loading data from S3')
-            }
-        
-        parquet_converter = ParquetConverter()
-        parquet_buffer = parquet_converter.to_parquet(df)
+        parquet_buffer = io.BytesIO()
+        df.to_parquet(parquet_buffer, index=False)
 
         print("JSON has been converted into parquet")
         
         s3_uploader = S3Uploader()
         s3_uploader.upload(parquet_buffer)
-
-        print("Parquet file uploaded into S3 successfully")
 
         return {
             'statusCode': 200,
