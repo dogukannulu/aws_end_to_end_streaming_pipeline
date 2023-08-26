9ccad188278a85bd665ab7b83249057ec8060009
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-26 23:51:03 +0300

    add populated_price

---
+++ athena_queries/populated_price.sql
@@ -0,0 +1,12 @@
+WITH mean_price AS (
+    SELECT AVG(price) AS mean_price_value
+    FROM 'glue-etl-books-parquet-dogukan-ulu.clean_books_parquet'
+    WHERE price IS NOT NULL
+)
+
+SELECT
+    title,
+    num_reviews,
+    availability,
+    IFNULL(price, (SELECT mean_price_value FROM mean_price)) as populated_price
+FROM 'glue-etl-books-parquet-dogukan-ulu.clean_books_parquet'
