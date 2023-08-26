d2a05a18d98dcc968e53a2f4c6a0270f6a53f947
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-27 00:00:23 +0300

    add partition by

---
+++ athena_queries/num_reviews_partitioned.sql
@@ -0,0 +1,16 @@
+WITH max_price AS (
+    SELECT
+        title,
+        price,
+        num_reviews,
+        ROW_NUMBER() OVER(PARTITION BY num_reviews ORDER BY price DESC) as price_row_number
+    FROM 'glue-etl-books-parquet-dogukan-ulu.clean_books_parquet'
+)
+
+
+SELECT
+    title,
+    price,
+    num_reviews
+FROM max_price
+WHERE price_row_number = 1
