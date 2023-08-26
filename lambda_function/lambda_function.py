a3599f1e3a224940cdcbc88d974883e9feb2daac
Dogukan Ulu <dogukannulu@gmail.com>
2023-08-26 18:01:39 +0300

    define only directory

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -53,7 +53,7 @@ class S3Uploader:
     def __init__(self):
         self.s3 = boto3.client('s3')
         self.target_s3_bucket = "aws-glue-dirty-books-parquet-dogukan-ulu"
-        self.target_s3_key = "books_parquet/books.parquet"
+        self.target_s3_key = "books_parquet/"
 
     def upload(self, buffer):
         """
