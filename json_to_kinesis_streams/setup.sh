153f1d4e8a55d25f83e2eb52fe511c4752634330
Dogukan Ulu <91257958+dogukannulu@users.noreply.github.com>
2023-08-27 13:51:08 +0300

    120 -> 180

--- json_to_kinesis_streams/setup.sh
+++ json_to_kinesis_streams/setup.sh
@@ -42,7 +42,7 @@ execute_python_script() {
     local json_to_kinesis_streams_script="json_to_kinesis_streams.py"
     local stream_name="json-to-kinesis-streams-dogukan-ulu"
     local interval=1
-    local max_records=120
+    local max_records=180
     local json_url="https://raw.githubusercontent.com/dogukannulu/send_data_to_aws_services/main/json_to_kinesis_streams/books_data.json"
     
     log_message "Executing the Python script"
